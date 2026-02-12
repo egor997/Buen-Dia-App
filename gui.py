d1323d66b43c4b9c798aca84510dc37896e63f54
egor997 <egorgavrileyko@gmail.com>
2026-02-12 22:05:03 +0100

    moved table functions to table.py implemented functions to load data while starting and for saving data after changing the cell

--- gui.py
+++ gui.py
@@ -1,9 +1,7 @@
 import sys
-from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 from ui_gui import Ui_MainWindow
-from datetime import datetime
-# TODO: add the function which defines the month and changes the amount of days to display ### datetime lib
+from table import TableManager
 # TODO: add to readme note to delete the code for starting on fedora
 # ============================================= #
 # Added only to start the code on my fedora
@@ -14,49 +12,20 @@ os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "1"
 
 
 class MainWindow(QMainWindow):
+    """Initializing MainWindow to display it properly"""
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.set_up_table_items()
+        self.table_manager = TableManager(self.ui.tableWidget)
 
-    def set_up_table_items(self):
-            rows = self.ui.tableWidget.rowCount()
-            columns = self.ui.tableWidget.columnCount()
-            for row in range(rows):
-                for column in range(columns-1):
-                    item = QTableWidgetItem()
-                    item.setFlags(QtCore.Qt.ItemIsUserCheckable |
-                                QtCore.Qt.ItemIsEnabled |
-                                QtCore.Qt.ItemIsSelectable)
-                    item.setCheckState(QtCore.Qt.Unchecked)
-                    item.setTextAlignment(QtCore.Qt.AlignCenter)
-
-                    self.ui.tableWidget.setItem(row, column, item)
-                streak_item = QTableWidgetItem()
-                streak_item.setTextAlignment(QtCore.Qt.AlignCenter)
-                self.ui.tableWidget.setItem(row, columns-1, streak_item)
-
-            self.ui.tableWidget.itemChanged.connect(self.count_streak)
-
-    # set up streak column
-    def count_streak(self, item):
-        streak_column = self.ui.tableWidget.columnCount()-1
-        # check if it's not the streak column
-        if item.column() == streak_column:
-            return 0
-
-        today_day = datetime.today().day
-        streak = 0
-        for column in range(today_day):
-            table_item = self.ui.tableWidget.item(item.row(), column)
-            if table_item and table_item.checkState() == QtCore.Qt.Checked:
-                streak += 1
-            else:
-                streak = 0
-        streak_item = self.ui.tableWidget.item(item.row(), streak_column)
-        streak_item.setText(str(streak))
-        return streak
+    def closeEvent(self, event):
+        # ensure any recent changes are persisted before exit
+        try:
+            self.table_manager.save_table_state()
+        except Exception:
+            pass
+        super().closeEvent(event)
 
 
 def main():
